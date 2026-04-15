52af16ddee9b1be18cbc7407169e314c0d378df6
dominicochieng254 <dominicochieng254@gmail.com>
2026-04-15 16:35:33 +0300

    first commit

---
+++ app/src/main/java/com/stiles/young/ui/theme/screens/authentication/signup/SignUpScreen.kt
@@ -0,0 +1,148 @@
+package com.stiles.young.ui.theme.screens.authentication.signup
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.stiles.young.R
+import com.stiles.young.ui.theme.screens.authentication.login.LottieAnimationWidget
+
+@Composable
+fun SignUpScreen() {
+    var nameInput by remember { mutableStateOf(TextFieldValue("")) }
+    var emailInput by remember { mutableStateOf(TextFieldValue("")) }
+    var passwordInput by remember { mutableStateOf(TextFieldValue("")) }
+    var confirmPasswordInput by remember { mutableStateOf(TextFieldValue("")) }
+    var isVisible by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LottieAnimationWidget(R.raw.king, 200.dp)
+        
+        Text(
+            text = "CREATE ACCOUNT",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // Name input
+        OutlinedTextField(
+            value = nameInput,
+            onValueChange = { nameInput = it },
+            label = { Text(text = "Full Name") },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Person, contentDescription = "Name")
+            },
+            shape = RoundedCornerShape(32.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedTextColor = Color.Blue,
+                focusedBorderColor = Color.Magenta
+            )
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // Email input
+        OutlinedTextField(
+            value = emailInput,
+            onValueChange = { emailInput = it },
+            label = { Text(text = "Email Address") },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Email, contentDescription = "Email")
+            },
+            shape = RoundedCornerShape(32.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedTextColor = Color.Blue,
+                focusedBorderColor = Color.Red
+            )
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // Password input
+        OutlinedTextField(
+            value = passwordInput,
+            onValueChange = { passwordInput = it },
+            label = { Text(text = "Password") },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.outline_password_24),
+                    contentDescription = "Password"
+                )
+            },
+            trailingIcon = {
+                IconButton(onClick = { isVisible = !isVisible }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.outline_visibility_off_24),
+                        contentDescription = "Toggle Visibility"
+                    )
+                }
+            },
+            shape = RoundedCornerShape(32.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedTextColor = Color.Blue,
+                focusedBorderColor = Color.Green
+            )
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // Confirm Password
+        OutlinedTextField(
+            value = confirmPasswordInput,
+            onValueChange = { confirmPasswordInput = it },
+            label = { Text(text = "Confirm Password") },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.outline_password_24),
+                    contentDescription = "Confirm Password"
+                )
+            },
+            shape = RoundedCornerShape(32.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedTextColor = Color.Blue,
+                focusedBorderColor = Color.Cyan
+            )
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = { /* Handle Sign Up */ },
+            shape = RoundedCornerShape(32.dp)
+        ) {
+            Text(text = "SIGN UP")
+        }
+    }
+}
